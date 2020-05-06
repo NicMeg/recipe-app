1d7722515c14329c03ff97c90c417d28c259ea3f
GorginZ <georgia.z.leng@gmail.com>
2020-05-06 16:39:35 +1000

    scaffolding for recipes added many to one relationship for recipes/users

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_06_044051) do
+ActiveRecord::Schema.define(version: 2020_05_06_062231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.string "summary"
+    t.text "method"
+    t.text "body"
+    t.boolean "published"
+    t.bigint "user_id_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id_id"], name: "index_recipes_on_user_id_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
