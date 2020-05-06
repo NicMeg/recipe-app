3436685cdf9faf227b1ecd3d72984243d89f4978
Joshua Gilmore <djsounddog@gmail.com>
2020-05-06 15:48:59 +1000

    User authorization and default public access.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,12 +6,25 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
+    #  user ||= User.new # guest user (not logged in)
+
+    can :read, Recipe, public: true
+    can :read, Comments, public: true
+
+    if user.present?
+      if user.is_admin?
+        can :manage, :all
+      end
+
+      if user.is_moderator?
+        can :manage, [:comments, :recipes]
+      end
+
+      # Standard user access
+      can :create, Recipe
+      can [:edit, :update, :destroy], Recipe, user_id: user.id
+      can [:edit, :update, :destroy], User, user_id: user.id
+    end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
