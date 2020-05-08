66f46d630c455ea5888833483322e97add6d9c9b
Joshua Gilmore <djsounddog@gmail.com>
2020-05-08 11:32:02 +1000

    Repaired Bootstrap installation. Polished index view for MVP.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,9 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+
+// Load all bootstrap JS
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require bootstrap
+//= require_tree .
